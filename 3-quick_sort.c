ee74b55244700afa96cf46112a339ea57b3db130
erickadikah <erickadikah2030@gmail.com>
2022-10-12 12:10:18 -0700

    task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,87 @@
+#include "sort.h"
+
+/**
+ * swap - swaps two numbers
+ * @num1: first number
+ * @num2: second number
+ */
+void swap(int *num1, int *num2)
+{
+	int tmp = *num1;
+	*num1 = *num2;
+	*num2 = tmp;
+}
+
+/**
+ * partition - find partition position
+ * @array: array of interger
+ * @loww: lower bound of array
+ * @high: higher bound of array
+ * @size: number of elememnts in @array
+ *
+ * Return: position of pivot
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	/* elect the last right element as pivot*/
+	int pivot = array[high];
+	/* points to the greater element */
+	int ptr = low;
+	int j;
+	/*
+	 * traversing acreoss each element in an array and
+	 * compare them with pivot
+	 */
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			if (ptr != j)
+			{
+				/* if elememnt smaller than pivot is found
+				 * swap it with the greater element poited by i
+				 */
+				swap(&array[ptr], &array[j]);
+				print_array(array, size);
+			}
+			ptr++;
+		}
+	}
+	/*swap the pivot element with the greater element at i */
+	if (ptr != high)
+	{
+		swap(&array[ptr], &array[high]);
+		print_array(array, size);
+	}
+	return (ptr);
+}
+
+/**
+ * partition - find partition position
+ * @array: array of interger
+ * @loww: lower bound of array
+ * @high: higher bound of array
+ * @size: number of elememnts in @array
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+	int pivot;
+
+	if(low < high)
+	{
+		pivot = partition(array, low, high, size);
+		quicksort(array, low, pivot - 1, size);
+		quicksort(array, pivot + 1, high, size);
+	}
+}
+/**
+ * quick_sort quick sorts an array
+ * @array: array to sort
+ * @size: size of array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	quicksort(array, 0, size - 1, size);
+}
